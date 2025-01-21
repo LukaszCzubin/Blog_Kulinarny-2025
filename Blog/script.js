7acfab109d22eed5acaa00ec0eb9fe1e892e9a20
a <a>
2025-01-21 10:34:23 +0100

    v1

---
+++ Blog/script.js
@@ -0,0 +1,60 @@
+const hearts = document.getElementById('serce');
+const licznik= document.getElementById('licznik');
+hearts.addEventListener('click' , ()=> {
+    licznik.textContent = parseInt(licznik.textContent) + 1 ;
+}
+);
+const hearts2 = document.getElementById('serce2');
+const licznik2= document.getElementById('licznik2');
+hearts2.addEventListener('click' , ()=> {
+    licznik2.textContent = parseInt(licznik2.textContent) + 1 ;
+}
+); 
+const hearts3 = document.getElementById('serce3');
+const licznik3= document.getElementById('licznik3');
+hearts3.addEventListener('click' , ()=> {
+    licznik3.textContent = parseInt(licznik3.textContent) + 1 ;
+}
+); 
+const hearts10 = document.getElementById('serce3');
+const licznik10= document.getElementById('licznik3');
+hearts10.addEventListener('click' , ()=> {
+    licznik10.textContent = parseInt(licznik10.textContent) + 1 ;
+}
+); 
+const hearts4 = document.getElementById('serce3');
+const licznik4= document.getElementById('licznik3');
+hearts4.addEventListener('click' , ()=> {
+    licznik4.textContent = parseInt(licznik4.textContent) + 1 ;
+}
+); 
+const hearts5 = document.getElementById('serce3');
+const licznik5= document.getElementById('licznik3');
+hearts5.addEventListener('click' , ()=> {
+    licznik5.textContent = parseInt(licznik5.textContent) + 1 ;
+}
+); 
+const hearts6 = document.getElementById('serce3');
+const licznik6= document.getElementById('licznik3');
+hearts6.addEventListener('click' , ()=> {
+    licznik6.textContent = parseInt(licznik6.textContent) + 1 ;
+}
+); 
+const hearts7 = document.getElementById('serce3');
+const licznik7= document.getElementById('licznik3');
+hearts7.addEventListener('click' , ()=> {
+    licznik7.textContent = parseInt(licznik7.textContent) + 1 ;
+}
+); 
+const hearts8 = document.getElementById('serce3');
+const licznik8= document.getElementById('licznik3');
+hearts8.addEventListener('click' , ()=> {
+    licznik8.textContent = parseInt(licznik8.textContent) + 1 ;
+}
+); 
+const hearts9 = document.getElementById('serce3');
+const licznik9= document.getElementById('licznik3');
+hearts9.addEventListener('click' , ()=> {
+    licznik9.textContent = parseInt(licznik9.textContent) + 1 ;
+}
+); 
